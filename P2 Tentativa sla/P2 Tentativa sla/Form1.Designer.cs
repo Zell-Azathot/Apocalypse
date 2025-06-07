7b41ce9b6efb08cb92cdf2bf8493f0c0c9335623
Isabella <Isabe@GRANMULTTIVERSE>
2025-06-07 14:13:35 -0300

    Tentativa de Commit, de novo, depois de muitas muitas vezes.

---
+++ P2 Tentativa sla/P2 Tentativa sla/Form1.Designer.cs
@@ -0,0 +1,80 @@
+﻿namespace P2_Tentativa_sla
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            txtUsuario = new TextBox();
+            txtSenha = new TextBox();
+            button1 = new Button();
+            SuspendLayout();
+            // 
+            // txtUsuario
+            // 
+            txtUsuario.Location = new Point(26, 38);
+            txtUsuario.Name = "txtUsuario";
+            txtUsuario.Size = new Size(305, 23);
+            txtUsuario.TabIndex = 0;
+            // 
+            // txtSenha
+            // 
+            txtSenha.Location = new Point(26, 87);
+            txtSenha.Name = "txtSenha";
+            txtSenha.Size = new Size(305, 23);
+            txtSenha.TabIndex = 1;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(26, 130);
+            button1.Name = "button1";
+            button1.Size = new Size(72, 27);
+            button1.TabIndex = 2;
+            button1.Text = "Logar";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.Black;
+            ClientSize = new Size(800, 450);
+            Controls.Add(button1);
+            Controls.Add(txtSenha);
+            Controls.Add(txtUsuario);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox txtUsuario;
+        private TextBox txtSenha;
+        private Button button1;
+    }
+}
