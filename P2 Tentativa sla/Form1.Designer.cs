3f14acd4c22e8190a4353801b73fde8d453454d4
Isabella <Isabe@GRANMULTTIVERSE>
2025-06-07 15:19:56 -0300

    Acho que finalmente foi.

---
+++ P2 Tentativa sla/Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace P2_Tentativa_sla
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            txtUsuario = new TextBox();
+            txtSenha = new TextBox();
+            button1 = new Button();
+            label1 = new Label();
+            label2 = new Label();
+            SuspendLayout();
+            // 
+            // txtUsuario
+            // 
+            txtUsuario.Location = new Point(26, 38);
+            txtUsuario.Name = "txtUsuario";
+            txtUsuario.Size = new Size(305, 23);
+            txtUsuario.TabIndex = 0;
+            txtUsuario.TextChanged += txtUsuario_TextChanged;
+            // 
+            // txtSenha
+            // 
+            txtSenha.Location = new Point(26, 87);
+            txtSenha.Name = "txtSenha";
+            txtSenha.Size = new Size(305, 23);
+            txtSenha.TabIndex = 1;
+            txtSenha.TextChanged += txtSenha_TextChanged;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(26, 130);
+            button1.Name = "button1";
+            button1.Size = new Size(72, 27);
+            button1.TabIndex = 2;
+            button1.Text = "Logar";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.ForeColor = Color.White;
+            label1.Location = new Point(26, 20);
+            label1.Name = "label1";
+            label1.Size = new Size(50, 15);
+            label1.TabIndex = 3;
+            label1.Text = "Usúario:";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.ForeColor = Color.White;
+            label2.Location = new Point(26, 69);
+            label2.Name = "label2";
+            label2.Size = new Size(42, 15);
+            label2.TabIndex = 4;
+            label2.Text = "Senha:";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.Black;
+            ClientSize = new Size(361, 188);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(button1);
+            Controls.Add(txtSenha);
+            Controls.Add(txtUsuario);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox txtUsuario;
+        private TextBox txtSenha;
+        private Button button1;
+        private Label label1;
+        private Label label2;
+    }
+}
---
+++ P2 Tentativa sla/Form1.cs
@@ -0,0 +1,30 @@
+namespace P2_Tentativa_sla
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void txtUsuario_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtSenha_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
