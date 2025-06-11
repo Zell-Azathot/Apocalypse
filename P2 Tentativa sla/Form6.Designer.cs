588233c1771c1d7056568fd5c747319dc8e65016
Isabella <Isabe@GRANMULTTIVERSE>
2025-06-11 16:35:48 -0300

    iinicio ao forms6

--- P2 Tentativa sla/Form6.Designer.cs
+++ P2 Tentativa sla/Form6.Designer.cs
@@ -30,7 +30,22 @@
         {
             label1 = new Label();
             txtCPF = new TextBox();
+            btnCliente = new Button();
+            label2 = new Label();
+            txtNome = new TextBox();
+            label3 = new Label();
+            txtNomeProduto = new TextBox();
+            btnProduto = new Button();
+            label4 = new Label();
+            txtPreco = new TextBox();
+            label5 = new Label();
+            numericUpDown1 = new NumericUpDown();
+            dataGridView1 = new DataGridView();
+            txtPrecoTotal = new TextBox();
+            label6 = new Label();
             button1 = new Button();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
             SuspendLayout();
             // 
             // label1
@@ -49,14 +64,137 @@
             txtCPF.Name = "txtCPF";
             txtCPF.Size = new Size(269, 23);
             txtCPF.TabIndex = 1;
+            txtCPF.TextChanged += txtCPF_TextChanged;
+            // 
+            // btnCliente
+            // 
+            btnCliente.Location = new Point(296, 33);
+            btnCliente.Name = "btnCliente";
+            btnCliente.Size = new Size(89, 22);
+            btnCliente.TabIndex = 2;
+            btnCliente.Text = "Buscar cliente";
+            btnCliente.UseVisualStyleBackColor = true;
+            btnCliente.Click += btnCliente_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.ForeColor = SystemColors.Control;
+            label2.Location = new Point(21, 75);
+            label2.Name = "label2";
+            label2.Size = new Size(100, 15);
+            label2.TabIndex = 3;
+            label2.Text = "Nome do Cliente:";
+            // 
+            // txtNome
+            // 
+            txtNome.Location = new Point(21, 93);
+            txtNome.Name = "txtNome";
+            txtNome.ReadOnly = true;
+            txtNome.Size = new Size(269, 23);
+            txtNome.TabIndex = 4;
+            txtNome.TextChanged += txtNome_TextChanged;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.ForeColor = SystemColors.Control;
+            label3.Location = new Point(21, 129);
+            label3.Name = "label3";
+            label3.Size = new Size(106, 15);
+            label3.TabIndex = 5;
+            label3.Text = "Nome do Produto:";
+            // 
+            // txtNomeProduto
+            // 
+            txtNomeProduto.Location = new Point(21, 147);
+            txtNomeProduto.Name = "txtNomeProduto";
+            txtNomeProduto.Size = new Size(269, 23);
+            txtNomeProduto.TabIndex = 6;
+            txtNomeProduto.TextChanged += txtNomeProduto_TextChanged;
+            // 
+            // btnProduto
+            // 
+            btnProduto.Location = new Point(296, 148);
+            btnProduto.Name = "btnProduto";
+            btnProduto.Size = new Size(108, 22);
+            btnProduto.TabIndex = 7;
+            btnProduto.Text = "Buscar Produto";
+            btnProduto.UseVisualStyleBackColor = true;
+            btnProduto.Click += btnProduto_Click;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.ForeColor = SystemColors.Control;
+            label4.Location = new Point(18, 188);
+            label4.Name = "label4";
+            label4.Size = new Size(103, 15);
+            label4.TabIndex = 8;
+            label4.Text = "Preço do Produto:";
+            // 
+            // txtPreco
+            // 
+            txtPreco.Location = new Point(21, 206);
+            txtPreco.Name = "txtPreco";
+            txtPreco.ReadOnly = true;
+            txtPreco.Size = new Size(269, 23);
+            txtPreco.TabIndex = 9;
+            txtPreco.TextChanged += txtPreco_TextChanged;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.ForeColor = SystemColors.Control;
+            label5.Location = new Point(16, 247);
+            label5.Name = "label5";
+            label5.Size = new Size(135, 15);
+            label5.TabIndex = 11;
+            label5.Text = "Quantidade do Produto:";
+            // 
+            // numericUpDown1
+            // 
+            numericUpDown1.Location = new Point(21, 265);
+            numericUpDown1.Name = "numericUpDown1";
+            numericUpDown1.Size = new Size(45, 23);
+            numericUpDown1.TabIndex = 12;
+            numericUpDown1.ValueChanged += numericUpDown1_ValueChanged;
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(410, 12);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.Size = new Size(378, 411);
+            dataGridView1.TabIndex = 13;
+            dataGridView1.CellContentClick += dataGridView1_CellContentClick;
+            // 
+            // txtPrecoTotal
+            // 
+            txtPrecoTotal.Location = new Point(21, 316);
+            txtPrecoTotal.Name = "txtPrecoTotal";
+            txtPrecoTotal.ReadOnly = true;
+            txtPrecoTotal.Size = new Size(269, 23);
+            txtPrecoTotal.TabIndex = 15;
+            txtPrecoTotal.TextChanged += txtPrecoTotal_TextChanged;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.ForeColor = SystemColors.Control;
+            label6.Location = new Point(18, 298);
+            label6.Name = "label6";
+            label6.Size = new Size(132, 15);
+            label6.TabIndex = 14;
+            label6.Text = "Preço Total do Produto:";
             // 
             // button1
             // 
-            button1.Location = new Point(296, 33);
+            button1.Location = new Point(21, 369);
             button1.Name = "button1";
-            button1.Size = new Size(89, 22);
-            button1.TabIndex = 2;
-            button1.Text = "Buscar cliente";
+            button1.Size = new Size(108, 22);
+            button1.TabIndex = 16;
+            button1.Text = "Adicionar";
             button1.UseVisualStyleBackColor = true;
             // 
             // Form6
@@ -66,10 +204,25 @@
             BackColor = Color.Black;
             ClientSize = new Size(800, 450);
             Controls.Add(button1);
+            Controls.Add(txtPrecoTotal);
+            Controls.Add(label6);
+            Controls.Add(dataGridView1);
+            Controls.Add(numericUpDown1);
+            Controls.Add(label5);
+            Controls.Add(txtPreco);
+            Controls.Add(label4);
+            Controls.Add(btnProduto);
+            Controls.Add(txtNomeProduto);
+            Controls.Add(label3);
+            Controls.Add(txtNome);
+            Controls.Add(label2);
+            Controls.Add(btnCliente);
             Controls.Add(txtCPF);
             Controls.Add(label1);
             Name = "Form6";
             Text = "Form6";
+            ((System.ComponentModel.ISupportInitialize)numericUpDown1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -78,6 +231,19 @@
 
         private Label label1;
         private TextBox txtCPF;
+        private Button btnCliente;
+        private Label label2;
+        private TextBox txtNome;
+        private Label label3;
+        private TextBox txtNomeProduto;
+        private Button btnProduto;
+        private Label label4;
+        private TextBox txtPreco;
+        private Label label5;
+        private NumericUpDown numericUpDown1;
+        private DataGridView dataGridView1;
+        private TextBox txtPrecoTotal;
+        private Label label6;
         private Button button1;
     }
 }
--- P2 Tentativa sla/Form6.cs
+++ P2 Tentativa sla/Form6.cs
@@ -12,9 +12,62 @@ namespace P2_Tentativa_sla
 {
     public partial class Form6 : Form
     {
+        string Caminho1 = Path.Combine(Application.StartupPath, "pedidos.txt");
+        string Caminho2 = Path.Combine(Application.StartupPath, "clientes.txt");
+        string Caminho3 = Path.Combine(Application.StartupPath, "produtos.txt");
         public Form6()
         {
             InitializeComponent();
+            if (!File.Exists(Caminho1))
+            {
+                File.WriteAllText(Caminho1, "" + Environment.NewLine);
+                return;
+            }
+        }
+
+        private void txtCPF_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtNome_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtNomeProduto_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtPreco_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtPrecoTotal_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCliente_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnProduto_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
         }
     }
 }
